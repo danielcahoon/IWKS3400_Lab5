3fd210e961f5562b991e28d59e193dac72f86346
Danny Cahoon <daniel.cahoon@gmail.com>
2017-03-25 14:30:27 -0600

    Started adding door animation

---
+++ Lab5/IWKS3400_Lab5/Assets/Scripts/OverWorld/OpenBossDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBossDoor : MonoBehaviour {
+
+
+	private Animator anim;
+
+	void Start()
+	{
+		anim = gameObject.GetComponent<Animator>();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		anim.SetBool("BossDoorOpen", true);
+	}
+}
