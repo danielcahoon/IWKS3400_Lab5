3d5218eabefcc85565cd0bfcfbe8c625b6584128
Danny Cahoon <daniel.cahoon@gmail.com>
2017-03-25 23:28:57 -0600

    started on tutorial text

---
+++ Lab5/IWKS3400_Lab5/Assets/Scripts/Stage01/TutorialTextToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTextToggle : MonoBehaviour {
+
+
+	public GameObject text;
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		text.SetActive(true);
+	}
+	public void OnTriggerExit2D(Collider2D col)
+	{
+		text.SetActive (false);
+	}
+}
