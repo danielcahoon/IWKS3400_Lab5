e1f6b59bae2be63483c2c2156cf7bc2b06ab5283
Danny Cahoon <daniel.cahoon@gmail.com>
2017-03-26 22:42:14 -0600

    Pause Screen implemented

---
+++ Lab5/IWKS3400_Lab5/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour {
+	public GameObject pauseUI;
+
+	private bool paused = false;
+
+	void Start()
+	{
+		pauseUI.SetActive (false);
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown ("Pause")) {
+			paused = !paused;
+		}
+
+		if (paused) {
+			pauseUI.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			pauseUI.SetActive (false);
+			Time.timeScale = 1;
+		}
+
+	}
+	public void Resume()
+	{
+		paused = false;
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene (Application.loadedLevel);
+	}
+	public void MainMenu()
+	{
+		SceneManager.LoadScene ("MainMenuScene");
+	}
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
