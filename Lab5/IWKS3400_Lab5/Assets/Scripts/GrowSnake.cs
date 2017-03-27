1886a1ff0009cdf621ebf9d136f5eb4b2cb38b44
Jake <jacob.jolly@ucdenver.edu>
2017-03-26 18:28:38 -0600

    .

--- Lab5/IWKS3400_Lab5/Assets/Scripts/GrowSnake.cs
+++ Lab5/IWKS3400_Lab5/Assets/Scripts/GrowSnake.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GrowSnake : MonoBehaviour {
-    public AudioClip hitSound;
-    private AudioSource source;
-    private float volLowRange = .5f;
-    private float volHighRange = 1.0f;
-    void Awake()
-    {
-        source = GetComponent<AudioSource>();
-    }
+
     void OnTriggerEnter2D(Collider2D collide)
 	{
 		if (collide.CompareTag ("Player")) {
-            float vol = Random.Range(volLowRange, volHighRange);
-            source.PlayOneShot(hitSound, vol);
             transform.localScale = new Vector3 (-1, 1, 1);
 		}
 	}
